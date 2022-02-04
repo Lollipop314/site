cf55c69bb0b894976327a6c59afee51a0c7e123e
Lollipop314 <howiethelollipop314@gmail.com>
2022-02-03 23:16:50 -0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,476 @@
+let entries = [
+	{
+		name: "A Tale of Two Lovebirds",
+		ev: "BOFXVII",
+		genre: "Romantic Ballad",
+		modes: "7K",
+		credits: "",
+		link: "https://venue.bmssearch.net/romanticbms/1",
+		dl: "https://mega.nz/file/Pb42GDAL#nnkjg0-R65hndRe0MpqvuLBUK3Tpg-mERWSA4yP6SWc",
+		listen: "https://soundcloud.com/lollipop-144989227/romantic-bms-a-tale-of-two-lovebirds",
+		img: "two_lovebirds.jpg"
+	},
+	{
+		name: "Illuminated By Moonlight",
+		ev: "BOFXVII",
+		genre: "Piano",
+		modes: "7K, 9K, 14K, 18K",
+		credits: "[Charts by various people; BGI: Dossy]",
+		link: "https://manbow.nothing.sh/event/event.cgi?action=More_def&num=337&event=137",
+		dl: "https://mega.nz/file/nTYggRYI#LiylwWs8UTrFNJ6rmugERiba371wG_bPQzhAV53S4KQ",
+		listen: "https://soundcloud.com/lollipop-144989227/bofxvii-illuminated-by-moonlight",
+		img: "illuminated_moonlight.jpg"
+	},
+	{
+		name: "Diachronic Reflection",
+		ev: "BOFXVII",
+		genre: "Piano",
+		modes: "7K",
+		credits: "[Some charts by vi]",
+		link: "https://manbow.nothing.sh/event/event.cgi?action=More_def&num=303&event=137",
+		dl: "https://mega.nz/file/HXgWFDBa#gYesDwTmK0sDWQv4b7KkjL6Dci2B2sxfjWhPFLYv114",
+		listen: "https://soundcloud.com/lollipop-144989227/bofxvii-diachronic-reflection",
+		img: "diachronic.jpg"
+	},
+	{
+		name: "thinking about past days",
+		ev: "BOFXVII",
+		genre: "Instrumental",
+		modes: "7K",
+		credits: "[Some charts by vi]",
+		link: "https://manbow.nothing.sh/event/event.cgi?action=More_def&num=268&event=137",
+		dl: "https://mega.nz/file/vXZUTJiS#PVPfe1f8D8MdqO6oK8gWZu3wj-93MYhP-AGlpT0ONQg",
+		listen: "https://soundcloud.com/lollipop-144989227/bofxvii-thinking-of-past-days",
+		img: "past_days.jpg"
+	},
+	{
+		name: "a vast field of corn",
+		ev: "beatissimo",
+		genre: "piano",
+		modes: "14K",
+		credits: "[From my work '3 Little Preludes']",
+		link: "https://manbow.nothing.sh/event/event.cgi?action=More_def&num=6&event=136",
+		dl: "https://mega.nz/file/eSpVQALJ#wdc6uk8JRo4eLMI9xlWolxZYqjDqBddomuiRfrEVMFQ",
+		listen: "https://soundcloud.com/lollipop-144989227/beatissimo-a-vast-field-of-corn",
+		img: "cornfield.jpg"
+	},
+	{
+		name: "to make him moan like that (emojis)",
+		ev: "beatissimo",
+		genre: "i can tell",
+		modes: "14K",
+		credits: "[warning: stupid—released under the alias it was bussin]",
+		link: "https://manbow.nothing.sh/event/event.cgi?action=More_def&num=5&event=136",
+		dl: "https://cdn.discordapp.com/attachments/269267264660832257/863527398091915264/bussin.zip",
+		listen: "unavailable",
+		img: "bussin.jpg"
+	},
+	{
+		name: "Praetorium",
+		ev: "beatissimo",
+		genre: "Ensemble",
+		modes: "14K",
+		credits: "",
+		link: "https://manbow.nothing.sh/event/event.cgi?action=More_def&num=4&event=136",
+		dl: "https://mega.nz/file/TTAR2YZQ#VWmgyvzXo0fpVak0rCyQ0NIqL66AafBeUz3F3U23Clk",
+		listen: "https://soundcloud.com/lollipop-144989227/praetorium",
+		img: "praetorium.jpg"
+	},
+	{
+		name: "calming patterns to soothe the mind",
+		ev: "MUMEI Academy 2021",
+		genre: "Piano Etude",
+		modes: "7K",
+		credits: "[With respect to Alexandre de la Fontaine]",
+		link: "https://manbow.nothing.sh/event/event.cgi?action=More_def&num=40&event=135",
+		dl: "https://mega.nz/file/XOpEDYTJ#uDnjdEizWdJnrba7zN-Oem9xThVkrfXkHp3hURRB7Pw",
+		listen: "https://soundcloud.com/lollipop-144989227/mumei-academy-2021-calming-patterns-to-soothe-the-mind",
+		img: "calming_patterns.jpg"
+	},
+	{
+		name: "ye olde marine snow",
+		ev: "2HDBMS",
+		genre: "Medieval",
+		modes: "7K, 14K",
+		credits: "[Charts by Sobrem]",
+		link: "https://manbow.nothing.sh/event/event.cgi?action=More_def&num=7&event=134",
+		dl: "https://mega.nz/file/KGhEmCDb#p0OswQh2KhsZE_FW5DV3Jzs5eblqcktVPf-JIjW1cb4",
+		listen: "https://soundcloud.com/lollipop-144989227/2hdbms-ye-olde-marine-snow",
+		img: "olde_snow.jpg"
+	},
+	{
+		name: "the ocean sure is vast, isn't it?",
+		ev: "PABAT! 2021",
+		genre: "along the shoreline",
+		modes: "7K, 14K",
+		credits: "",
+		link: "http://k-bms.com/party_pabat/party.jsp?board_num=21&num=15",
+		dl: "http://mega.nz/file/OKx1BY6I#2Yaw-ofIKDZtLr_FXOMXftCcmYs88aPno-XNysJqzz4",
+		listen: "https://soundcloud.com/lollipop-144989227/the-ocean-sure-is-vast-isnt-it",
+		img: "ocean_vast.jpg"
+	},
+	{
+		name: "were you there, when they crucified my lord?",
+		ev: "第三回BMS衆議院選",
+		genre: "christian hymn",
+		modes: "7K",
+		credits: "",
+		link: "https://venue.bmssearch.net/bmsshuin3/5",
+		dl: "https://mega.nz/file/eXolmaIY#OBjFb9zyKcJWnpIJUYZdJZySG_ua6Hd9jBgP7qRiWm4",
+		listen: "https://soundcloud.com/lollipop-144989227/were-you-there-when-they-crucified-my-lord",
+		img: "wyt.jpg"
+	},
+	{
+		name: "Laplacian Chasm",
+		ev: "\"Nancyatte\" A-1 Climax -Just Kidding-",
+		genre: "Schranz",
+		modes: "7K",
+		credits: "[Released under the alias L-アヴァリス; some charts by AYhaz]",
+		link: "http://starkey.ivory.ne.jp/nanA1/nanA1/detail/view/1/",
+		dl: "https://drive.google.com/file/d/1Ugpr3B6kdVZj2aAu98x3hVfKTYOIimgR/view?usp=sharing",
+		listen: "https://soundcloud.com/lollipop-144989227/laplacian-chasm",
+		img: "laplacian_chasm.jpg"
+	},
+	{
+		name: "marine snow -orchestral arrange-",
+		ev: "BMSをたくさん作るぜ'20",
+		genre: "Orchestral",
+		modes: "7K",
+		credits: "[Original tracks by LaValse and Medwyn Goodall]",
+		link: "https://venue.bmssearch.net/bmstukuru2020/9",
+		dl: "https://mega.nz/file/mbQzjQqJ#tBqRCSu-QVAR44X5saDd89Y8Br0BoDEFX5ilayuLdvg",
+		listen: "https://soundcloud.com/lollipop-144989227/bms20-marine-snow-orchestral-arrange",
+		img: "marine_snow.jpg"
+	},
+	{
+		name: "uhhh guys my prostate just imploded",
+		ev: "BOFXVI",
+		genre: "Juke / Footwork",
+		modes: "7K",
+		credits: "[Released under the alias exacerbated pinecone]",
+		link: "http://manbow.nothing.sh/event/event.cgi?action=More_def&num=125&event=133",
+		dl: "https://mega.nz/file/vOwz2ICT#Re_idp7-b95tnCQOyYgIf_o03SXeFNogVTb7pdkqO5A",
+		listen: "https://soundcloud.com/lollipop-144989227/bofxvi-uhhh-guys-my-prostate-just-imploded",
+		img: "prostate.jpg"
+	},
+	{
+		name: "memories of a distant time (Radio Edit)",
+		ev: "BOFXVI",
+		genre: "Trance",
+		modes: "7K",
+		credits: "",
+		link: "http://manbow.nothing.sh/event/event.cgi?action=More_def&num=236&event=133",
+		dl: "https://mega.nz/file/7HIDWSLZ#WZYuWkLI81oGNgii5Rv4ih_bM0t6dg71R46aIjHVn_g",
+		listen: "https://soundcloud.com/lollipop-144989227/bofxvi-memories-of-a-distant-time",
+		img: "distant_time.jpg"
+	},
+	{
+		name: "Cloudy Skies",
+		ev: "BOFXVI",
+		genre: "Introspective Trance",
+		modes: "7K",
+		credits: "",
+		link: "http://manbow.nothing.sh/event/event.cgi?action=More_def&num=140&event=133",
+		dl: "https://mega.nz/file/7HoCABiJ#OKRbzTz4FdU4BpkfUFsC4fgwAIPyann5jhHQ1XYl5D4",
+		listen: "https://www.youtube.com/watch?v=zYKN3qDqVME",
+		img: "cloudy_skies.jpg"
+	},
+	{
+		name: "o tempo está bom; vamos à praia~",
+		ev: "WW_battle",
+		genre: "bossa nova",
+		modes: "7K",
+		credits: "[BGA: Alexandre de la Fontaine]",
+		link: "http://manbow.nothing.sh/event/event.cgi?action=More_def&num=26&event=132",
+		dl: "https://mega.nz/file/bGRwFQbC#jRxClhY8sHBENi5qFd7FPAONYXKChu8uNSilFAlqxns",
+		listen: "https://soundcloud.com/lollipop-144989227/ww_battle-o-tempo-esta-bom-vamos-a-praia",
+		img: "vamos_a_praia.jpg"
+	},
+	{
+		name: "on a sunny afternoon",
+		ev: "A-1 ClimaX 10th",
+		genre: "piano beats",
+		modes: "7K",
+		credits: "",
+		link: "http://manbow.nothing.sh/event/event.cgi?action=More_def&num=47&event=131",
+		dl: "https://mega.nz/file/7TRCyKAY#uTOFv6lz_zH9CKcc1Y1GhqaaHSSBFtlMLykj9_FM_ig",
+		listen: "https://soundcloud.com/lollipop-144989227/a-1-climax-10th-on-a-sunny-afternoon",
+		img: "sunny_afternoon.jpg"
+	},
+	{
+		name: "a day in the park",
+		ev: "SPRING 9KEYS BLOSSOM 2020",
+		genre: "urban lifestyle",
+		modes: "9K",
+		credits: "",
+		link: "http://manbow.nothing.sh/event/event.cgi?action=More_def&num=3&event=130",
+		dl: "https://mega.nz/file/XagihQJS#6GWsoKHO9VZ7bs6YejxRxENpVxrQeITgTVkXjkkIBM0",
+		listen: "https://soundcloud.com/lollipop-144989227/a-day-in-the-park",
+		img: "a_day_in_the_park.jpg"
+	},
+	{
+		name: "Lullaby for a Lost Child",
+		ev: "PABAT! 2020",
+		genre: "Berceuse",
+		modes: "7K, 14K",
+		credits: "[Dedicated to my violin instructor]",
+		link: "http://k-bms.com/party_pabat/party.jsp?board_num=20&num=58",
+		dl: "http://mega.nz/#!LX41EQRR!XR2qk_KOBSzSwqPjmetBC_yi6_IDcXzWTEwqPPCrdNs",
+		listen: "https://soundcloud.com/lollipop-144989227/pabat-2020-lullaby-for-a-lost-child",
+		img: "berceuse.jpg"
+	},
+	{
+		name: "forget me not",
+		ev: "PABAT! 2020",
+		genre: "instrumental",
+		modes: "7K, 14K",
+		credits: "",
+		link: "http://k-bms.com/party_pabat/party.jsp?board_num=20&num=14",
+		dl: "http://mega.nz/#!mXwBlSbD!USYjvGjZbdG0kFDaQgMOydg7pvSu3um_ZSwR55nFyTM",
+		listen: "https://soundcloud.com/lollipop-144989227/forget-me-not",
+		img: "forget_me_not.jpg"
+	},
+	{
+		name: "you can't change a thing... anymore",
+		ev: "Be-Music West II",
+		genre: "Electro Pop",
+		modes: "7K",
+		credits: "[BGA: capitano]",
+		link: "http://starkey.ivory.ne.jp/bemusicwest/bmw2/detail/view/6/",
+		dl: "https://mega.nz/#!vTg2AKCK!L83SP3RULBSwtPjIloKwIOlcZDPaAyX9BrNfXs79G9A",
+		listen: "https://soundcloud.com/lollipop-144989227/you-cant-change-a-thing-anymore",
+		img: "thing_anymore.jpg"
+	},
+	{
+		name: "sad piano no.3",
+		ev: "BOFoonXV",
+		genre: "piano mood",
+		modes: "7K, 14K",
+		credits: "",
+		link: "http://manbow.nothing.sh/event/event.cgi?action=More_def&num=34&event=128",
+		dl: "https://mega.nz/#!TOgwTQZb!w9uJuj6oeaGb-sYjmxIfcbCLmufMhLPSCs2YvPHRmB4",
+		listen: "https://soundcloud.com/lollipop-144989227/bofoonxvsad-piano-no3-2020-edit",
+		img: "sad_piano_no3.jpg"
+	},
+	{
+		name: "speed gaming",
+		ev: "BOFoonXV",
+		genre: "combo based ranking system",
+		modes: "7K",
+		credits: "[warning: stupid—released under the alias osu man]",
+		link: "http://manbow.nothing.sh/event/event.cgi?action=More_def&num=42&event=128",
+		dl: "https://mega.nz/#!rehAlCKJ!hyW9jN4PGk7tsJXOQzGl_aiDocyNFka8c93n63Z_UKg",
+		listen: "unavailable",
+		img: "speed_gaming.jpg"
+	},
+	{
+		name: "journey across an endless sky",
+		ev: "BOFXV",
+		genre: "Artcore",
+		modes: "7K",
+		credits: "[Inspired by daisan's Skyblue Eden]",
+		link: "http://manbow.nothing.sh/event/event.cgi?action=More_def&num=119&event=127",
+		dl: "https://mega.nz/#!iGAQHabA!uUbdoIQ6WSSBlFfOwvd9yrIotkTZM3dUiXRLLk1kIjI",
+		listen: "https://soundcloud.com/lollipop-144989227/journey-across-an-endless-sky",
+		img: "endless_sky.jpg"
+	},
+	{
+		name: "Odeon",
+		ev: "BMSパッケージ企画「ゆるゆる」",
+		genre: "Tango Brasiliero",
+		modes: "7K, 9K",
+		credits: "[Original by Ernesto Nazareth]",
+		link: "https://sites.google.com/view/yuruyurubms/phase-1",
+		dl: "https://mega.nz/#!nK4RRYbb!rqhizcbg0tPenLTE3fQQWqdIqBah-jPfnc_R0lhgT1w",
+		listen: "https://soundcloud.com/lollipop-144989227/odeon",
+		img: "odeon.jpg"
+	},
+	{
+		name: "Summer Sunset",
+		ev: "第二回BMS衆議院選",
+		genre: "Instrumental",
+		modes: "7K",
+		credits: "",
+		link: "https://venue.bmssearch.net/bmsshuin2/19",
+		dl: "https://mega.nz/#!iP4ilAiR!RMayyGpc0tATBNxY5Pk44Vu2nzVuGAjFUJxFei_PFYE",
+		listen: "https://soundcloud.com/lollipop-144989227/summer-sunset",
+		img: "summer_sunset.jpg"
+	},
+	{
+		name: "Ostinato Quartet for Pizzicato Strings",
+		ev: "A-1 ClimaX 9th",
+		genre: "Chamber",
+		modes: "7K",
+		credits: "",
+		link: "http://manbow.nothing.sh/event/event.cgi?action=More_def&num=28&event=126",
+		dl: "https://mega.nz/#!XH4RTKRS!DtHnnl8jMwbL0RksS8Ge9xo453GVmqa0DFBcdXF4Wuc",
+		listen: "https://soundcloud.com/lollipop-144989227/ostinato-quartet-for-pizzicato-strings",
+		img: "ostinato_quartet.jpg"
+	},
+	{
+		name: "Everlasting Sunrise",
+		ev: "無名戦対抗戦",	
+		genre: "Trance & Strings",
+		modes: "7K, 14K",
+		credits: "",
+		link: "https://event.yaruki0.net/VSevent/impression/36",
+		dl: "https://mega.nz/#!iKx2zSRZ!r_btLSBq5fB0J2Aq5B6VOYM06ynW-QRSqe4vz3t-tyE",
+		listen: "https://soundcloud.com/lollipop-144989227/everlasting-sunrise",
+		img: "everlasting_sunrise.jpg"
+	},
+	{
+		name: "Variations on a Theme from Korobeiniki",
+		ev: "no event - april fools' 2019",
+		genre: "Theme and Variations",
+		modes: "7K",
+		credits: "[Released under the alias piano man]",
+		link: "https://pupuly.nekokan.dyndns.info/bms/v/150",
+		dl: "https://mega.nz/#!nXR23KTC!GEOhV6LqvDkM-Ksg7JBak0xA2B1FD5yYVt-n3uXs3uk",
+		listen: "unavailable",
+		img: "tetris.jpg"
+	},
+	{
+		name: "untitled violin",
+		ev: "PABAT! 2019",
+		genre: "Violin Solo",
+		modes: "7K",
+		credits: "[Released under the alias c12h22o11]",
+		link: "http://k-bms.com/party_pabat/party.jsp?board_num=19&num=18",
+		dl: "http://mega.nz/#!bWxikK7a!6ZqKceIa0m9FjGioaXtnk48s7XfA44y-affJchKXFw4",
+		listen: "https://www.newgrounds.com/audio/listen/978015",
+		img: "untitled_violin.jpg"
+	},
+	{
+		name: "sad piano no.2",
+		ev: "PABAT! 2019",
+		genre: "Sad Piano",
+		modes: "7K",
+		credits: "[Collab with AYhaz]",
+		link: "http://k-bms.com/party_pabat/party.jsp?board_num=19&num=29",
+		dl: "http://mega.nz/#!m4Zg2AQB!vzrEH6RP4z-OoAWmRxbCfsiStDnAkwJlXrF97ma635k",
+		listen: "https://soundcloud.com/lollipop-144989227/sad-piano-no2",
+		img: "sad_piano_no2.jpg"
+	},
+	{
+		name: "gmaj9",
+		ev: "PABAT! 2019",
+		genre: "lo-fi",
+		modes: "7K",
+		credits: "",
+		link: "https://k-bms.com/party_pabat/party2019.jsp?board_num=19&num=11",
+		dl: "http://mega.nz/#!eLJ3VKxS!9se8l0CKg5-EDWLaXwiT6NTMmDa3UEfoo3RQT1TutF4",
+		listen: "https://soundcloud.com/lollipop-144989227/gmaj9",
+		img: "gmaj9.jpg"
+	},
+	{
+		name: "Elina (Jazz Quartet Remix)",
+		ev: "PABAT! 2019",
+		genre: "Jazz Quartet",
+		modes: "7K",
+		credits: "[BGA: AYhaz]",
+		link: "https://k-bms.com/party_pabat/party2019.jsp?board_num=19&num=27",
+		dl: "http://mega.nz/#!GDwUXarZ!dj8LZ6Qx3FiHEYEILSOYBQPVYASTRxjRZaUyW134L50",
+		listen: "https://soundcloud.com/lollipop-144989227/elina-jazz-quartet-remix",
+		img: "elina_jazz_rmx.jpg"
+	},
+	{
+		name: "The Jolly Man's Rag (BMS Edit)",
+		ev: "BMSをいっぱい作る2019",
+		genre: "Ragtime Piano",
+		modes: "7K",
+		credits: "",
+		link: "https://venue.bmssearch.net/ippaitukuru2019/26",
+		dl: "https://mega.nz/#!mSY0hCLD!fVQ5SM0sS7nBdYK4monheIbBqSngioe2Bt5nv26Mpgo",
+		listen: "unavailable",
+		img: "jolly_man_rag.jpg"
+	},
+	{
+		name: "Minuet for Harpsichord and Oboe", 
+		ev: "OLDSKOOL VS FUTURE 後夜祭",
+		genre: "Oldskool Baroque",
+		modes: "7K, 5K",
+		credits: "",
+		link: "http://manbow.nothing.sh/event/event.cgi?action=More_def&num=4&event=122",
+		dl: "https://drive.google.com/file/d/1btL-pSyIPZ0PQiqMOttjLXXrXv1Tz_I3/view?usp=sharing",
+		listen: "https://soundcloud.com/lollipop-144989227/minuet-for-harpsichord-and-oboe",
+		img: "minuet.jpg"
+	},
+	{ 
+		name: "sad piano no.1", 
+		ev: "no event - pupuply",
+		genre: "Piano Solo",
+		modes: "7K",
+		credits: "", // Credits are formatted a tad bit differently
+		link: "https://pupuly.nekokan.dyndns.info/bms/v/132",
+		dl: "https://mega.nz/#!qH4g2QBR!Bgr-H6sK8tfs0mf0ZLCnuSQZdE2QA3-V4aCgepmSIyI",
+		listen: "https://www.newgrounds.com/audio/listen/822050",
+		img: "sad_piano_no1.jpg"
+	}
+]
+
+function generateBMSTable(table, stuff){
+	let q = 0
+  	for(let entry of stuff){
+    	let row = table.insertRow();
+    	let cell = row.insertCell();
+
+    	let title = document.createElement("H6");
+    	title.appendChild(document.createTextNode(entry["name"] + " 【" + entry["ev"] + "】"));
+    	cell.appendChild(title);
+
+    	if(entry["credits"] == ""){
+			let line_1 = document.createElement("P");
+    		line_1.appendChild(document.createTextNode("Genre: " + entry["genre"] + "\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0Keymodes: " + entry["modes"]));
+    		cell.appendChild(line_1);
+    	}
+    	else {
+    		let line_1 = document.createElement("P");
+    		line_1.appendChild(document.createTextNode("Genre: " + entry["genre"] + "\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0Keymodes: " + entry["modes"] + "\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0" + entry["credits"]));
+    		cell.appendChild(line_1);
+    	}
+
+    	let shit = [
+    		["BMS Link: ", "link"], 
+    		["Direct Download: ", "dl"],  
+    		["Listen: ", "listen"]
+    	];
+
+    	for(const pair of shit){
+    		console.log(pair);
+    		let line = document.createElement("P");
+    		line.appendChild(document.createTextNode(pair[0]));
+
+    		let a = document.createElement("A");
+    		let a_text = document.createTextNode(entry[pair[1]]);
+    		if(entry[pair[1]] == "unavailable"){a.setAttribute('href', "https://www.youtube.com/watch?v=y9K18CGEeiI");}
+        	else{a.setAttribute('href', entry[pair[1]]);}
+        	a.setAttribute('target', "_blank");
+        	a.appendChild(a_text);
+        	line.appendChild(a);
+
+    		cell.appendChild(line);
+    	}
+
+    	let cell_2 = row.insertCell();
+    	let click = document.createElement("A");
+    	click.setAttribute('href', entry["link"]);
+    	click.setAttribute('target', "_blank");
+
+    	let img = document.createElement("IMG");
+    	img.src = "img/" + entry["img"];
+    	img.setAttribute('height', "200px");
+    	click.appendChild(img);
+    	cell_2.appendChild(click);
+
+    	q += 1;
+
+	    /*for (key in entry) {
+	      let cell = row.insertCell();
+	      let text = document.createTextNode(entry[key]);
+	      cell.appendChild(text);
+	    }*/
+  	}
+  	console.log(q);
+}
+
+generateBMSTable(document.querySelector("table"), entries);
